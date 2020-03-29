b2f36349d3069885e8b7e6cc4a183214cb4eb2d8
ycg31 <ycg31@qq.com>
2020-03-29 14:56:27 +0800

    Update 222.sh

--- 222.sh
+++ 222.sh
@@ -1,2 +1,4 @@
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.100.222/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.22/g' openwrt/package/base-files/files/bin/config_generate
+#2. Add firmware timestamp
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d-%H%M%S)-/g' openwrt/include/image.mk
