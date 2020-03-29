198da4fd5e7ae0283c5413be40bb74540112e511
ycg31 <ycg31@qq.com>
2020-03-29 16:47:40 +0800

    Update 22.sh

--- 22.sh
+++ 22.sh
@@ -3,6 +3,6 @@ sed -i 's/192.168.1.1/192.168.100.22/g' openwrt/package/base-files/files/bin/con
 #2. Modify default gatewy
 sed -i '108a \                                set network.$1.gateway='192.168.100.1'' config_generate
 #3. Modify default DNS
-sed -i '108a \                                set network.$1.dns='222.172.200.68\ 223.5.5.5\ 8.8.8.8'' config_generate
+sed -i '109a \                                set network.$1.dns='222.172.200.68\ 223.5.5.5\ 8.8.8.8'' config_generate
 #4. Add firmware timestamp
 sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d-%H%M%S)-/g' openwrt/include/image.mk
