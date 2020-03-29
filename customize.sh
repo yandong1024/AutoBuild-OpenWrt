9e862e96225ce4295b9706f96ca33e9313bd219c
ycg31 <ycg31@qq.com>
2020-03-29 14:56:47 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#=================================================
-# Description: DIY script
-# Lisence: MIT
-# Author: eSirPlayground
-#=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.100.222/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.22/g' openwrt/package/base-files/files/bin/config_generate
+#2. Add firmware timestamp
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d-%H%M%S)-/g' openwrt/include/image.mk
