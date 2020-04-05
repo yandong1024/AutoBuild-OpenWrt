c7e494676558deb245c7000ff25f8c99575a36b7
ycg31 <ycg31@qq.com>
2020-04-06 00:30:18 +0800

    Create Lienol_22.sh

---
+++ Lienol_22.sh
@@ -0,0 +1,8 @@
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.100.22/g' openwrt/package/base-files/files/bin/config_generate
+#2. Modify default gatewy
+sed -i '112a \                                set network.$1.gateway='192.168.100.1'' openwrt/package/base-files/files/bin/config_generate
+#3. Modify default DNS
+sed -i '113a \                                set network.$1.dns='222.172.200.68\ 223.5.5.5\ 8.8.8.8'' openwrt/package/base-files/files/bin/config_generate
+#4. Add firmware timestamp
+sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +%Y%m%d-%H%M%S)-/g' openwrt/include/image.mk
